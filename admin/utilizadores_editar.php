d4df5bf278c646aec3d503e549083af49600fe41
João Ribeiro <business@sys4soft.com>
2018-01-30 09:47:48 +0000

    Add files via upload

--- admin/utilizadores_editar.php
+++ admin/utilizadores_editar.php
@@ -45,7 +45,56 @@
             $erro = true;
             $mensagem = 'Não foram encontrados dados do utilizador.';
         }
-    }    
+    }  
+    
+    // ==============================================================
+    // POST
+    // ==============================================================
+    if($_SERVER['REQUEST_METHOD']== 'POST'){
+        
+        //buscar os dados das texts
+        $nome = $_POST['text_nome'];
+        $email = $_POST['text_email'];
+
+        //verificações - verifica se existe outro utilizador com o mesmo email
+        $parametros = [
+            ':id_utilizador' => $id_utilizador,
+            ':email'         => $email
+        ];        
+
+        $temp = $gestor->EXE_QUERY('SELECT * FROM utilizadores
+                                    WHERE id_utilizador <> :id_utilizador
+                                    AND email = :email', $parametros);
+        if(count($temp) != 0){
+            $erro = true;
+            $mensagem = 'Já existe outro utilizador com o mesmo email.';
+        }
+
+        // ========================================
+        // atualiza os dados na base de dados
+        if(!$erro){
+            $parametros = [
+                ':id_utilizador' => $id_utilizador,
+                ':nome'          => $nome,
+                ':email'         => $email,
+                ':atualizado_em' => DATAS::DataHoraAtualBD()
+            ];  
+            
+            $gestor->EXE_NON_QUERY(
+                'UPDATE utilizadores SET
+                nome  = :nome,
+                email = :email,
+                atualizado_em = :atualizado_em
+                WHERE id_utilizador = :id_utilizador',$parametros);
+            
+            //sucesso
+            $sucesso = true;
+            $mensagem = 'Dados atualizados com sucesso.';
+
+            $parametros = [':id_utilizador' => $id_utilizador];
+            $dados_utilizador = $gestor->EXE_QUERY('SELECT * FROM utilizadores WHERE id_utilizador = :id_utilizador', $parametros);
+        }
+    }
 ?>
 
 <!-- erro de permissão -->
@@ -66,9 +115,14 @@
         </div>
 
     <?php else : ?>
-
-    <!-- sucesso na alteração dos dados -->
+    
     <!-- apresenta uma mensagem de sucesso -->
+    <?php if($sucesso): ?>
+        <div class="alert alert-success text-center">
+            <?php echo $mensagem ?>
+        </div>
+    <?php endif; ?>
+    
 
     <!-- formulário com os dados para alteração -->
     <div class="container">
@@ -83,7 +137,34 @@
                                 <label>Utilizador:</label>
                                 <p><strong><?php echo $dados_utilizador[0]['utilizador'] ?></strong></p>
 
+                                <!-- nome completo -->
+                                <div class="form-group">
+                                    <label>Nome:</label>
+                                    <input type="text"
+                                        name="text_nome"
+                                        class="form-control"
+                                        pattern=".{3,50}"
+                                        title="Entre 3 e 50 caracteres."
+                                        placeholder="<?php echo $dados_utilizador[0]['nome'] ?>"
+                                        required>
+                                </div>
 
+                                <!-- email -->
+                                <div class="form-group">
+                                    <label>Email:</label>
+                                    <input type="email"
+                                        name="text_email"
+                                        class="form-control"
+                                        pattern=".{3,50}"
+                                        title="Entre 3 e 50 caracteres."
+                                        placeholder="<?php echo $dados_utilizador[0]['email'] ?>"
+                                        required>
+                                </div>
+
+                                <div class="text-center">
+                                    <a href="?a=utilizadores_gerir" class="btn btn-primary btn-size-150">Cancelar</a>
+                                    <button class="btn btn-primary btn-size-150">Atualizar</button>
+                                </div>
 
 
                             </div>
